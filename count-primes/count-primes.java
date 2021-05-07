5ada7ea3130da0766a8ee808ece71bd51d673e33
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-05-07 16:02:43 +0530

    Time: 68 ms (21.65%), Space: 57.3 MB (5.00%) - LeetHub

---
+++ count-primes/count-primes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countPrimes(int n) {
+        int  arr[] = new int[n];
+        int count =0;
+        if(n==0 || n==1)
+            return 0;
+        
+        Arrays.fill(arr,1);
+        arr[0]=0;
+        arr[1]=0;
+        for(int i=2;i*i<=n;i++)
+        {
+            
+            if(arr[i]==1)
+            {
+                for(int j=i*i;j<n;j=j+i)
+                {
+                     arr[j]=0;     
+                }
+                
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==1)
+                count++;
+        }
+        return count;
+    }
+}
