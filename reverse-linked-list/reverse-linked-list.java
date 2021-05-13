17a1d34dc2d296ce7b034b15eb1449c054da680f
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-05-13 15:26:18 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (62.24%) - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        if(head == null)
+            return null;
+        ListNode next= curr.next;
+        while(curr.next!=null)
+        {
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            next = next.next;
+        }
+        curr.next = prev;
+        return curr;
+    }
+}
