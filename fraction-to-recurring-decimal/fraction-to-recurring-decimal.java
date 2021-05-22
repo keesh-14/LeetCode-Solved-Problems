cc2d5565692f538ecf15e0eaa7e0bf26cc8cbaff
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-05-22 19:14:57 +0530

    Time: 1 ms (99.94%), Space: 36.4 MB (59.57%) - LeetHub

--- fraction-to-recurring-decimal/fraction-to-recurring-decimal.java
+++ fraction-to-recurring-decimal/fraction-to-recurring-decimal.java
@@ -1,35 +1,37 @@
 class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        if (numerator == 0) return "0";
-        StringBuilder sb = new StringBuilder();
+       StringBuilder str = new StringBuilder();
+           if((numerator>0)^(denominator>0) && numerator!=0)
+             str.append("-");
         
-        if (numerator > 0 ^ denominator > 0) sb.append('-');
+        long num = (long)Math.abs(numerator);
+        long den = (long)Math.abs(denominator);
+        long q = (long)Math.abs(num/den);
+        long rem = (long)Math.abs(num%den);
+        str.append(q);
+        if(rem==0)
+        return str.toString();
+        str.append(".");
         
-        long n = Math.abs((long)numerator), d = Math.abs((long)denominator),
-             q = n/d , r = n%d ;
-        sb.append(q);
-        
-        if (r == 0) return sb.toString();
-        else {
-            sb.append('.');
-            r *= 10;
-        }
-        
-        HashMap<Long,Integer> map = new HashMap<>();
-        for (int start = sb.length(); r != 0; start++, r *= 10) {
-			//If this dividend has already appeared, stop and add "()"
-            int repetition = map.getOrDefault(r,-1);
-            if (repetition != -1) {
-                sb.insert(repetition, "(");
-                sb.append(')');
+        HashMap<Long,Integer> hm = new HashMap<Long,Integer>();
+        while(rem!=0)
+        {
+            if(hm.containsKey(rem))
+            {
+                str.insert(hm.get(rem),"(");
+                str.append(")");
                 break;
             }
-			//Store the index to note the start of repition for particular dividend
-            map.put(r,start);
-            q = r/d;
-            r = r%d;
-            sb.append(q);
+            hm.put(rem,str.length());
+            rem*=10;
+            q=Math.abs(rem/den);
+            rem = rem%den;
+           str.append(q); 
         }
-        return sb.toString();
+        return str.toString();
+        
+           
+          
     }
+    
 }
