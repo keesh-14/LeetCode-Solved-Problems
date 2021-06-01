a0aa39c4c5268689175455a2dd178b2c1d38d9af
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-06-01 22:04:54 +0530

    Time: 5 ms (98.20%), Space: 43.5 MB (94.81%) - LeetHub

--- sort-list/sort-list.java
+++ sort-list/sort-list.java
@@ -11,75 +11,65 @@
 class Solution {
     public ListNode merge(ListNode l1,ListNode l2)
     {
-        ListNode curr1=l1;
-        ListNode curr3=l2;
-    
-        if(l1==null && l2==null)
-            return null;
-        if(l1==null)
-            return l2;
-        if(l2==null)
-            return l1;
+        
+        ListNode curr1 = l1;
+        ListNode curr2 = l2;
         ListNode ptr = null;
-        if(curr1.val<curr3.val)
+        if(curr2.val>curr1.val)
         {
             ptr = curr1;
-            curr1=curr1.next;
-         
+            curr1 = curr1.next;
         }
         else 
         {
-            ptr = curr3;
-            curr3=curr3.next;
+            ptr = curr2;
+            curr2 = curr2.next;
         }
-        ListNode c =ptr;
-        while(curr3!=null && curr1!=null)
+        ListNode c = ptr;
+        while(curr1!=null && curr2!=null)
         {
-            if(curr3.val > curr1.val )
+            if(curr1.val>curr2.val)
+            {
+                ptr.next = curr2;
+                ptr = curr2;
+                curr2 = curr2.next;
+            }
+            else 
             {
                 ptr.next = curr1;
-                ptr = ptr.next;
+                ptr = curr1;
                 curr1 = curr1.next;
-            }else 
-            {
-                 ptr.next = curr3;
-                ptr = ptr.next;
-                curr3 = curr3.next;
             }
-        }
-        if(curr3!=null)
-        {
-            ptr.next = curr3;
-            
         }
         if(curr1!=null)
         {
             ptr.next = curr1;
-            
+        }
+        if(curr2!=null)
+        {
+            ptr.next = curr2;
         }
         return c;
     }
-    
-    public ListNode sortList(ListNode head) {
-    
+    public ListNode sortList(ListNode head)
+    {
+        if(head == null || head.next==null)
+            return head;
         
         ListNode curr1 = head;
         ListNode curr2 = head;
-        ListNode ptr = null;
-        ListNode curr3 = head;
-        if(head == null || head.next==null)
-            return head;
-        while(curr2!=null && curr2.next !=null && curr2.next.next!=null)
+        while(curr2!=null && curr2.next!=null && curr2.next.next!=null)
         {
             curr1 = curr1.next;
             curr2 = curr2.next.next;
         }
         ListNode curr = curr1.next;
         curr1.next = null;
-    
-        ListNode left = sortList(curr3);
+        curr2 = head;
+        ListNode left = sortList(curr2);
         ListNode right = sortList(curr);
-        return merge(left,right);
+        ListNode p = merge(left,right);
+        return p;
         
     }
 }
