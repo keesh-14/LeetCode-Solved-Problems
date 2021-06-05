52e9d23cf8484306f986a19dce83baa9045f3eda
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-06-05 18:10:32 +0530

    Time: 28 ms (24.50%), Space: 38.9 MB (31.74%) - LeetHub

---
+++ coin-change/coin-change.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int dp[][] = new int[coins.length+1][amount+1];
+        if(amount==0)
+            return 0;
+        if(coins.length == 1)
+        {
+            if( amount%coins[0]==0)
+                return amount/coins[0];
+            else 
+                return -1;
+        }
+        
+            
+        for(int i=1;i<coins.length+1;i++)
+        {
+            dp[i][0] = 0;
+        }
+        for(int i=0;i<amount+1;i++)
+        {
+            dp[0][i] = Integer.MAX_VALUE-1;
+        }
+        for(int i=1;i<amount+1;i++)
+        {
+            if(i%coins[0]==0)
+            dp[1][i] = i/coins[0];
+            else 
+                dp[1][i] = Integer.MAX_VALUE-1;
+        }
+        for(int i=2;i<coins.length+1;i++)
+        {
+            for(int j=1;j<amount+1;j++)
+            {
+                if(j>=coins[i-1])
+                    dp[i][j] =  Math.min(dp[i][j-coins[i-1]] + 1 ,dp[i-1][j]);
+                else 
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        if(dp[coins.length][amount]==Integer.MAX_VALUE -1)
+            return -1;
+        return dp[coins.length][amount];
+    }
+}
