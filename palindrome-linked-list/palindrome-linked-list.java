021defd542d9a834da12801e7a4d7c19b7256f16
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-06-25 15:52:51 +0530

    Time: 6 ms (65.65%), Space: 48.9 MB (78.03%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.java
+++ palindrome-linked-list/palindrome-linked-list.java
@@ -9,64 +9,50 @@
  * }
  */
 class Solution {
-    public boolean check(ListNode head1,ListNode head2)
+    public ListNode reverse(ListNode head)
     {
-        ListNode temp1=head1;
-        ListNode temp2 = head2;
-        while(temp2!=null)
+        if(head.next==null)
+            return head;
+        ListNode prev=null;
+        ListNode curr = head;
+        ListNode Next = head.next;
+        while(Next!=null)
         {
-            if(temp1.val!=temp2.val)
-            {
+            curr.next = prev;
+            prev = curr;
+            curr = Next;
+            Next=Next.next;
+        }
+        curr.next=prev;
+        prev = curr;
+        return prev; 
+    }
+    public boolean check(ListNode ptr1,ListNode ptr2)
+    {
+        while(ptr1!=null && ptr2!=null)
+        {
+            if(ptr1.val!=ptr2.val)
                 return false;
-            }
-            temp1=temp1.next;
-            temp2=temp2.next;
+            ptr1=ptr1.next;
+            ptr2=ptr2.next;
         }
         return true;
     }
-    public ListNode rev(ListNode head)
-    {
-        ListNode prev=null;
-        ListNode curr=head;
-        ListNode Next = curr.next;
-        while(Next!=null)
-        {
-            curr.next=prev;
-            prev = curr;
-            curr = Next;
-            Next = Next.next; 
-        }
-        curr.next=prev;
-        
-        return curr;
-    }
-    public boolean isPalindrome(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        ListNode f = head;
-        while(fast!=null && fast.next!=null)
-        {
-            slow=slow.next;
-            fast = fast.next.next;
-        }
-        if(head.next==null)
-            return true;
-        if(slow.val == head.val && head.next.next==null)
-            return true;
-        ListNode c = head;
-        int count=0;
-       while(c!=null)
-       {
-           c=c.next;
-           count++;
-       }
-        ListNode res;
-       if(count%2==0)
-          res = rev(slow);
-        else 
-          res = rev(slow.next);
-         
-        return check(f,res);
-       
+ public boolean isPalindrome(ListNode head) {
+     ListNode slow = head;
+     ListNode fast = head;
+     ListNode ptr1 = head;
+    ListNode  ptr2 =null;
+     if(head.next == null)
+         return true;
+     while(fast!=null && fast.next!=null && fast.next.next!=null)
+     {
+         fast=fast.next.next;
+         slow=slow.next;
+     }
+      ptr2 = slow.next;
+      slow.next = null;
+      ListNode p = reverse(ptr2);
+      return check(ptr1,p); 
     }
 }
