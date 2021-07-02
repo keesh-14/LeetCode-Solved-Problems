c1fc0a9560272c5773469dcfb0d969e7a74c575f
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-07-03 00:08:51 +0530

    Time: 2 ms (70.94%), Space: 39.3 MB (61.03%) - LeetHub

--- add-two-numbers/add-two-numbers.java
+++ add-two-numbers/add-two-numbers.java
@@ -10,20 +10,19 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode curr1 = l1;
-        ListNode curr2 = l2;
-         ListNode temp = new ListNode((curr1.val+curr2.val)%10);
-        
-        ListNode curr;
-        ListNode ptr;
-        curr = temp;
-        ptr = curr;
-            int c = (curr1.val+curr2.val)/10;
-        curr1=curr1.next;
-        curr2=curr2.next; 
-        while(curr1!=null && curr2!=null)
+        ListNode curr1 = l1; // creating the 1st pointer
+        ListNode curr2 = l2; // creating the 2nd pointer
+        ListNode temp = new ListNode((curr1.val+curr2.val)%10); //taking the last value
+        ListNode curr=null;  // pointer to iterate over the 1st linked list
+        ListNode ptr;  // pointer to keep the head preserved
+        curr = temp;   // pointer pointing to the 1st node
+        ptr = curr;    // storing head 
+        int c = (curr1.val+curr2.val)/10;  //taking out the carry
+        curr1=curr1.next; // moving in the 2nd iteration of the 1st list
+        curr2=curr2.next; //moving in the 2nd iteration of the 2nd list
+        while(curr1!=null && curr2!=null)  // // iterate till any one of them does not become null
         {
-            
+            // repeating the same process as above
             ListNode temp1 = new ListNode((curr1.val+curr2.val+c)%10);
             c = (curr1.val+curr2.val+c)/10;
             curr.next = temp1;
@@ -34,6 +33,7 @@ class Solution {
         }
         while(curr1!=null)
         {
+            //smae as above 
             ListNode temp1 = new ListNode((curr1.val+c)%10);
             c = (curr1.val+c)/10;
             curr.next = temp1;
@@ -43,6 +43,7 @@ class Solution {
         }
         while(curr2!=null)
         {
+            //same as above
             ListNode temp1 = new ListNode((curr2.val+c)%10);
             c = (curr2.val+c)/10;
             curr.next = temp1;
@@ -50,12 +51,12 @@ class Solution {
             curr2=curr2.next;
             
         }
-        if(c!=0)
+        if(c!=0) // for the last iteration if there is a carry over left
         {
             ListNode temp1 = new ListNode(c);
             curr.next=temp1;
         }
-        return ptr;
+        return ptr; // returning head
         
     }
 }
