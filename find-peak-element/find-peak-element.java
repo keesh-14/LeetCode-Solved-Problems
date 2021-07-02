72def7b2d145b4481010d66ce3c9032307f9a755
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-07-03 01:46:38 +0530

    Time: 0 ms (100.00%), Space: 38.1 MB (97.85%) - LeetHub

--- find-peak-element/find-peak-element.java
+++ find-peak-element/find-peak-element.java
@@ -1,20 +1,28 @@
 class Solution {
+   public int binary(int nums[],int low,int high)
+   {
+       while(low<=high)
+       {
+           int mid  = (low+high)/2;
+           if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+               return mid;
+           else if(nums[mid-1]>nums[mid])
+               high = mid -1;
+           else if(nums[mid+1]>nums[mid])
+               low = mid+1;
+               
+       }
+       return -1;
+   }
     public int findPeakElement(int[] nums) {
-        int i=0;
-        for( i=1;i<nums.length-1;i++)
-        {
-            if(nums[i]>nums[i+1] && nums[i]>nums[i-1])
-                break;
-            
-        }
-        if(nums.length == 1)
+        if(nums.length==1)
             return 0;
-        
         if(nums[0]>nums[1])
             return 0;
         else if(nums[nums.length-1]>nums[nums.length-2])
-            return nums.length -1;
-        else 
-            return i;
+            return nums.length-1;
+        int peek = binary(nums,1,nums.length-2);
+        return peek;
+        
     }
 }
