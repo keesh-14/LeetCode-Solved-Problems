c2f54502b633bc3169e5623dc0316a2a24fd6340
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-07-03 00:33:48 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (93.71%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,45 +1,41 @@
 class Solution {
-    public int binaryL(int nums[],int target)
-    {
-        int low=0;
-        int high = nums.length-1;
-        int ind = -1;
+    int a[] = new int[2];
+   public int binaryL(int low,int high,int[] nums,int target)
+   {
        while(low<=high)
        {
-           int mid = (low+high)/2;
-           if(nums[mid]==target)
-               ind = mid;
-            if(nums[mid]>=target)
-               high = mid -1;
+           int mid = (low + high)/2;
+           if(nums[mid]==target && (mid==0 || nums[mid-1]!=target))
+               return mid;
+           else if(nums[mid]>=target)
+               high = mid - 1;
            else 
                low = mid + 1;
+               
        }
-        return ind;
-    }
-    public int binaryR(int nums[],int target)
-    {
-        int low=0;
-        int high = nums.length-1;
-        int ind = -1;
+       return -1;
+   }
+    public int binaryR(int low,int high,int[] nums,int target)
+   {
        while(low<=high)
        {
-          int  mid = (low+high)/2;
-           if(nums[mid]==target)
-               ind = mid;
-           if(nums[mid]<=target)
-               low = mid +1;
-           else 
+           int mid = (low + high)/2;
+           if(nums[mid]==target && (mid==nums.length-1 || nums[mid+1]!=target))
+               return mid;
+           else if(nums[mid]>target)
                high = mid - 1;
+           else 
+               low = mid + 1;
+               
        }
-        return ind;
-    }
-    
+       return -1;
+   }
     public int[] searchRange(int[] nums, int target) {
-           int temp1 = binaryL(nums,target);
-        int temp2 = binaryR(nums,target);
-        int arr[] = new int[2];
-        arr[0]=temp1;
-        arr[1] = temp2;
-         return arr; 
+       
+        a[0] = binaryL(0,nums.length-1,nums,target);
+        a[1] = binaryR(0,nums.length-1,nums,target);
+        return a;
+        
+          
     }
 }
