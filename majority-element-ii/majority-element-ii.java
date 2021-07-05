11863680070ef014593017750d013c71ca35a6c2
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-07-05 19:06:43 +0530

    Time: 1 ms (99.93%), Space: 43.2 MB (22.76%) - LeetHub

--- majority-element-ii/majority-element-ii.java
+++ majority-element-ii/majority-element-ii.java
@@ -1,25 +1,29 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        int c1 = 0;
-        int c2 = 0;
-        int num1 = Integer.MIN_VALUE;
-        int num2 = Integer.MIN_VALUE;
-        
-        for(int i=0;i<nums.length;i++)
+        List<Integer> al = new ArrayList<Integer>();
+        int c1=0;
+        int c2=0;
+        int i=0;
+        int max1=Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        while(i<nums.length)
         {
-            if(nums[i]==num1)
-                c1++;
-            else if(nums[i]==num2)
-                c2++;
-            else if(c1==0)
+            if(nums[i]==max1)
             {
-                num1=nums[i];
                 c1++;
             }
-            else if(c2 == 0)
+            else if(nums[i]==max2)
             {
-                num2 = nums[i];
+                c2++;
+            }
+            else if(c1==0)
+            {
+                max1 = nums[i];
+                c1++;
+            }
+            else if(c2==0)
+            {
+                max2 = nums[i];
                 c2++;
             }
             else 
@@ -27,20 +31,22 @@ class Solution {
                 c1--;
                 c2--;
             }
+            i++;
         }
-        c1=0;
-        c2=0;
-        for(int i=0;i<nums.length;i++)
+        c1 = 0;
+        c2 = 0;
+        for(int c=0;c<nums.length;c++)
         {
-            if(nums[i]==num1)
+            if(nums[c]==max1)
                 c1++;
-            if(nums[i]==num2)
+            if(nums[c]==max2)
                 c2++;
+            
         }
         if(c1>nums.length/3)
-            al.add(num1);
+            al.add(max1);
         if(c2>nums.length/3)
-            al.add(num2);
+            al.add(max2);
         return al;
     }
 }
