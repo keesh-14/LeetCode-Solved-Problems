f4cb29356d2997e351957f5b94d63bee14839dc6
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-08-13 20:08:40 +0530

    Time: 512 ms (14.09%), Space: 232.2 MB (5.33%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,34 +1,55 @@
 class Solution {
+    public List<List<Integer>> twoSum(int nums[],int low,int high,int x)
+    {
+        List<Integer> res = new ArrayList<Integer>();
+        List<List<Integer>> al = new ArrayList<List<Integer>>();
+        while(low<high)
+        {
+            if(nums[low]+nums[high]==x)
+            {
+                
+                res.add(nums[low]);
+                res.add(nums[high]);
+                 al.add(new ArrayList<Integer>(res));
+                res.clear();
+                low++;
+                high--;
+            
+            }
+            else if(nums[low]+nums[high]<x)
+            {
+                low++;
+            }
+            else 
+                high--;
+            
+        }
+        return al;
+    }
    public List<List<Integer>> threeSum(int[] nums) {
-      Arrays.sort(nums);     
-       List<List<Integer>> res = new ArrayList<List<Integer>>();
-      for(int i=0;i<nums.length-2;i++)
-      {
-          if(i==0 || (i>0 && nums[i]!=nums[i-1]))
-          {
-              int low = i+1;
-              int high = nums.length -1;
-              int sum = 0-nums[i];
-              while(low<high)
-              {
-                  if(nums[low]+nums[high]==sum)
-                  {
-                      res.add((Arrays.asList(nums[low],nums[high],nums[i])));
-                  while(low<high && nums[low]==nums[low+1])
-                      low++;
-                  while(low<high && nums[high]==nums[high-1])
-                      high--;
-                      
-                      low++;
-                      high--;
-                  }else if(nums[high]+nums[low]>sum)
-                      high--;
-                  else 
-                      low++;
-              }
-          }
-      }
+       Arrays.sort(nums);
+      List<List<Integer>> res = new ArrayList<List<Integer>>();
+       for(int i=0;i<nums.length;i++)
+       {
+           List<List<Integer>> al  = twoSum(nums,i+1,nums.length-1,-nums[i]);
+           for(List<Integer> c: al)
+           {
+                c.add(0,nums[i]);
+                res.add(c);
+           }
         
-        return res;
+           
+       }
+       Set<List<Integer>> hm = new LinkedHashSet<List<Integer>>();
+       for(List<Integer> t:res)
+       {
+           hm.add(t);
+       }
+       res.clear();
+       for(List<Integer> s:hm)
+       {
+           res.add(s);
+       }
+       return res;
     }
 }
