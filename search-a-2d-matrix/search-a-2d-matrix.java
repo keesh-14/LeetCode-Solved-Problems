53430521b84fd0f5bafd412785c8fb9609d8efe3
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-08-17 18:24:06 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (19.96%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.java
+++ search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,36 +1,42 @@
 class Solution {
-    public boolean search(int arr[],int low,int high,int target)
+  
+    public boolean binary(int n,int matrix[][],int low,int high,int target)
     {
-        if(low<=high)
+        while(low<=high)
         {
            int  mid = (low+high)/2;
-            if(arr[mid] == target)
+            if(matrix[n][mid]==target)
                 return true;
-            else if(arr[mid]>target)
-                return search(arr,low,mid-1,target);
+            else if(matrix[n][mid]>target)
+            {
+                high = mid -1;
+            }
             else 
-                return search(arr,mid+1,high,target);
-            
+                low = mid + 1;
         }
         return false;
     }
     public boolean searchMatrix(int[][] matrix, int target) {
-        int top=0;
-        int row = 0;
-        int bottom= matrix.length-1;
-        while(top<=bottom)
-        {
-           int  mid = (top + bottom)/2;
-           if(matrix[mid][0]<=target && matrix[mid][matrix[mid].length-1]>=target)
-            {
-                row = mid;
-               break;
-            }
-            else if(matrix[mid][matrix[mid].length-1]<target)
-                top++;
-            else if(matrix[mid][0]>target)
-                bottom--;
+       int top=0;
+       int bottom = matrix.length-1;
+        int len = matrix[0].length-1;
+       while(top<=bottom)
+       {
+           int mid = (top + bottom)/2;
+           if(matrix[mid][len]>=target && matrix[mid][0]<=target)
+           {
+               if(binary(mid,matrix,0,matrix[mid].length-1,target))
+                   return true;
+               else 
+                   return false;
+           }
+           else if(matrix[mid][len]<target)
+           {
+               top =  mid + 1;
+           }
+           else if(matrix[mid][0]>target)
+               bottom = mid - 1;
         }
-        return search(matrix[row],0,matrix[row].length-1,target);
+        return false;
     }
 }
