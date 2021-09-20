e721b114ac0d11096f5e89d141123b2a9ca1ded6
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-09-20 13:57:46 +0530

    Time: 8 ms (52.37%), Space: 44.8 MB (28.92%) - LeetHub

---
+++ minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minTaps(int n, int[] ranges) {
+        
+        //furthest distance from a point towards right
+        int dist[]=new int[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            if(ranges[i]==0)//if range is 0 we ignore it
+                continue;      //*IMPORTANT* updates to i and may give ambiguity
+            int left=Math.max(0,i-ranges[i]);
+            int right=Math.min(n,i+ranges[i]);
+            dist[left]=Math.max(dist[left],right);
+        }
+        
+        int far=dist[0];//stores the farthest reachable distance with a tap
+        int tap=1;//stores the tap count
+        if(far==n)
+            return tap;
+        int next_tap=0;//stores the index of next tap
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i>far)//if we reach more than far it is unreacable
+                return -1;
+            next_tap=Math.max(next_tap,dist[i]);//updating next_tap index
+            if(next_tap==n)//if next tap is required at n it means we have watered all
+                return tap+1;
+            
+            //if we reach the max possible distance then we consider the next tap
+            if(far==i)
+            {
+                far=next_tap;
+                tap++;
+            }
+        }
+        return tap;
+    }
+}
