94482d86120586a0035f7fd9abba72fc8ce27f2d
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-09-22 17:44:58 +0530

    Time: 41 ms (52.14%), Space: 39.7 MB (58.53%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.java
+++ partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,42 +1,47 @@
 class Solution {
-    public boolean isSubset(int s1,int nums[])
-    {
-        boolean dp[][] = new boolean[nums.length+1][s1+1];
-        for(int i=1;i<s1+1;i++)
-        {
-            dp[0][i]=false;
-        }
-        for(int i=0;i<nums.length+1;i++)
-        {
-            dp[i][0] = true;
-        }
-        for(int i=1;i<nums.length+1;i++)
-        {
-            for(int j=1;j<s1+1;j++)
-            {
-                if(j>=nums[i-1])
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                else 
-                    dp[i][j] = dp[i-1][j];
-            }
-        }
-        return dp[nums.length][s1];
-        
-    }
+   public boolean isSubset(int[] nums,int sum)
+   {
+       boolean dp[][] = new boolean[nums.length+1][sum+1];
+       dp[0][0] = true;
+       for(int i=1;i<sum+1;i++)
+       {
+           dp[0][i] = false;
+           
+       }
+       for(int i=1;i<nums.length+1;i++)
+       {
+           dp[i][0] = true;
+       }
+       for(int i=1;i<nums.length+1;i++)
+       {
+           for(int j=1;j<sum+1;j++)
+           {
+               if(nums[i-1]<=j)
+               {
+                   dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+               }
+               else 
+               {
+                   dp[i][j] = dp[i-1][j];
+               }
+           }
+       }
+       return dp[nums.length][sum];
+   }
     public boolean canPartition(int[] nums) {
-       int s = 0;
-        if(nums.length == 1)
-            return false;
+        int sum = 0;
         for(int i=0;i<nums.length;i++)
         {
-            s = nums[i] + s;
+            sum = sum + nums[i];
+            
         }
-        if(s%2!=0)
+        if(sum%2!=0)
             return false;
-        int s1 = s/2;
-        if(isSubset(s1,nums))
-            return true;
         else 
-             return false;
+        {
+            
+            return isSubset(nums,sum/2);
+        }
+            
     }
 }
