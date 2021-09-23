bcfca3773b308e3f4039c1ffa26741b06cdca2f4
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-09-24 00:02:53 +0530

    Time: 4 ms (12.59%), Space: 39 MB (17.04%) - LeetHub

--- is-subsequence/is-subsequence.java
+++ is-subsequence/is-subsequence.java
@@ -1,21 +1,27 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if(s.length()==0)
-            return true;
-        
-        int x = 0;
-        int y=0;
-        while(y<t.length())
+        int dp[][] = new int[s.length()+1][t.length()+1];
+        for(int i=0;i<s.length()+1;i++)
         {
-            
-            if(s.charAt(x)==t.charAt(y))
-            {
-                x++;
-                if(x==s.length())
-                    return true;
-            }
-            y++;
+            dp[i][0] = 0;
         }
+        for(int i=0;i<t.length()+1;i++)
+        {
+            dp[0][i] = 0;
+        }
+        for(int i=1;i<s.length()+1;i++)
+        {
+            for(int j =1;j<t.length()+1;j++)
+            {
+                if(s.charAt(i-1)==t.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else 
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        if(s.length()==dp[s.length()][t.length()])
+            return true;
         return false;
     
     }
