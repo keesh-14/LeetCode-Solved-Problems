c5c29e7a14be3b79680f62b597cf2c9a0a33e853
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-09-23 14:34:08 +0530

    Time: 19 ms (56.11%), Space: 38.7 MB (55.09%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,28 +1,33 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int dp[][] = new int[coins.length+1][amount+1];
-        for(int i=0;i<amount+1;i++)
-        {
-            dp[0][i] = Integer.MAX_VALUE-1;
-        }
         for(int i=0;i<coins.length+1;i++)
         {
             dp[i][0] = 0;
         }
         for(int i=1;i<amount+1;i++)
         {
+            dp[0][i] = Integer.MAX_VALUE-1;
+        }
+        for(int i=1;i<amount+1;i++)
+        {
             if(i%coins[0]==0)
-            dp[1][i] = i/coins[0];
+            {
+                dp[1][i] = i/coins[0];
+            }
             else 
-            dp[1][i] = Integer.MAX_VALUE - 1;
+            {
+                dp[1][i] = Integer.MAX_VALUE-1;
+            }
         }
         for(int i=2;i<coins.length+1;i++)
         {
             for(int j=1;j<amount+1;j++)
             {
-                if(j>=coins[i-1])
+                if(coins[i-1]<=j)
                 {
-                    dp[i][j] = Math.min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
+                   
+                        dp[i][j] = Math.min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
                 }
                 else 
                 {
@@ -30,7 +35,7 @@ class Solution {
                 }
             }
         }
-        if(dp[coins.length][amount]==Integer.MAX_VALUE - 1)
+        if(dp[coins.length][amount]==Integer.MAX_VALUE-1)
             return -1;
         return dp[coins.length][amount];
     }
