d50d1fa71e9e3709dc93b6b4ab01c3b7edffdf44
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-11-09 19:51:42 +0530

    Time: 18 ms (6.51%), Space: 38.7 MB (11.59%) - LeetHub

---
+++ n-queens-ii/n-queens-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+    public boolean isSafe(int mat[][],int row,int col)
+    {
+        for(int i=0;i<col;i++)
+        {
+            if(mat[row][i]==1)
+                return false;
+            
+        }
+        
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--)
+        {
+            if(mat[i][j]==1)
+                return false;
+            
+        }
+         
+        for(int i=row,j=col;i<mat.length && j>=0;i++,j--)
+        {
+            if(mat[i][j]==1)
+                return false;
+        }
+        return true;
+            
+    }
+    public void solve(int mat[][],int col,int n,List<List<String>> res)
+    {
+        if(col==n)
+        {
+            ArrayList<String> al = new ArrayList<String>();
+            for(int i=0;i<n;i++)
+            {
+                String s ="";
+                for(int j=0;j<n;j++)
+                {
+                    s = s + (mat[i][j]==1?'Q':'.');
+                }
+                al.add(s);
+            }
+            res.add(al);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(isSafe(mat,i,col))
+            {
+                mat[i][col] = 1;
+                solve(mat,col+1,n,res);
+                mat[i][col] = 0;
+                
+            }
+            
+        }
+            
+    }
+    public int totalNQueens(int n) {
+        int mat[][] = new int[n][n];
+        List<List<String>> res = new ArrayList<List<String>>();
+        solve(mat,0,n,res);
+        return res.size();
+    }
+}
