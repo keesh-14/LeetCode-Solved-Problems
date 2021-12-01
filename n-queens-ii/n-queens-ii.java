73cd52aa20f963cffa73049d58331479fd82e9b4
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2021-12-01 14:20:31 +0530

    Time: 0 ms (100.00%), Space: 35.4 MB (97.44%) - LeetHub

--- n-queens-ii/n-queens-ii.java
+++ n-queens-ii/n-queens-ii.java
@@ -1,61 +1,33 @@
 class Solution {
-    public boolean isSafe(int mat[][],int row,int col)
+    int count = 0;
+    public void back(boolean col[],int row,boolean Ndiag[],boolean Rdiag[])
     {
-        for(int i=0;i<col;i++)
+        if(col.length==row)
         {
-            if(mat[row][i]==1)
-                return false;
-            
+            count++;
+            return;
         }
-        
-        for(int i=row,j=col;i>=0 && j>=0;i--,j--)
+        for(int i=0;i<col.length;i++)
         {
-            if(mat[i][j]==1)
-                return false;
-            
-        }
-         
-        for(int i=row,j=col;i<mat.length && j>=0;i++,j--)
-        {
-            if(mat[i][j]==1)
-                return false;
-        }
-        return true;
-            
-    }
-    public void solve(int mat[][],int col,int n,List<List<String>> res)
-    {
-        if(col==n)
-        {
-            ArrayList<String> al = new ArrayList<String>();
-            for(int i=0;i<n;i++)
+            if(col[i]==false && Ndiag[row+i]==false && Rdiag[row - i + col.length-1]==false)
             {
-                String s ="";
-                for(int j=0;j<n;j++)
-                {
-                    s = s + (mat[i][j]==1?'Q':'.');
-                }
-                al.add(s);
-            }
-            res.add(al);
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(isSafe(mat,i,col))
-            {
-                mat[i][col] = 1;
-                solve(mat,col+1,n,res);
-                mat[i][col] = 0;
+                col[i]=true;
+                Ndiag[row+i]=true;
+                Rdiag[row - i + col.length-1]=true;
+                back(col,row+1,Ndiag,Rdiag);
+                col[i]=false;
+                Ndiag[row+i]=false;
+                Rdiag[row - i + col.length-1]=false;
                 
             }
-            
         }
-            
     }
     public int totalNQueens(int n) {
-        int mat[][] = new int[n][n];
-        List<List<String>> res = new ArrayList<List<String>>();
-        solve(mat,0,n,res);
-        return res.size();
+        boolean col[] = new boolean[n];
+        boolean Ndiag[] = new boolean[2*n-1];
+        boolean Rdiag[] = new boolean[2*n-1];
+        
+        back(col,0,Ndiag,Rdiag);
+        return count;
     }
 }
