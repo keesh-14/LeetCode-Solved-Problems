8e665027453da19ca8e9d834ca70465c1d9e905a
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2022-01-14 17:06:06 +0530

    Time: 16 ms (6.79%), Space: 40.8 MB (5.54%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -0,0 +1,86 @@
+class Solution {
+    public int myAtoi(String s) {
+        String c = s.trim();
+        if(c.length()==0)
+            return 0;
+        
+        String st ="";
+        int x = c.length();
+        int i=0;
+        char str = ' ';
+        if(c.charAt(i)=='-')
+        {
+            str = '-';
+            i++;
+        }
+        else if(c.charAt(i)=='+')
+        {
+            str = '+';
+            i++;
+        }
+        while(i<c.length() && c.charAt(i)=='0')
+        {
+            i++;
+        }
+  
+        while(x>i && Character.isDigit(c.charAt(i)))
+        {
+            st = st + c.charAt(i);
+            i++;
+        }
+        if(st.length()==0)
+            return 0;
+        int k = st.length();
+        int j=0;
+        int n=0;
+   
+        if(k>10)
+        {
+          if(str=='-')
+          {
+              return Integer.MIN_VALUE;
+              
+          }
+            else 
+            {
+                return Integer.MAX_VALUE;
+            }
+        }
+        
+        while(j<k)
+        {
+           if((j==9 && n>Integer.MAX_VALUE/10)|| j>10)
+           {
+               if(str=='-')
+                   return Integer.MIN_VALUE;
+               else 
+                   return Integer.MAX_VALUE;
+           }
+           if(j==9 && n==Integer.MAX_VALUE/10)
+           {
+                   if(str=='-')
+                   {
+                       if(st.charAt(9)-'0'>=8)
+                           return Integer.MIN_VALUE;
+                       
+                   }
+                  else 
+                  {
+                      if(st.charAt(9)-'0'>=7)
+                          return Integer.MAX_VALUE;
+                  }
+                      
+            }
+
+            n = n*10 + (int)st.charAt(j)-'0';
+            j++;
+           
+        }
+        if(str=='-')
+            n = -n;
+      
+        
+        
+        return n;
+    }
+}
