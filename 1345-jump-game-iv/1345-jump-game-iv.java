8b8089c43e5350fbe0115e1249f38c9fdffe9c90
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2022-01-15 17:01:31 +0530

    Time: 37 ms (99.66%), Space: 48.1 MB (100.00%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        HashMap<Integer,List<Integer>> hm = new HashMap<Integer,List<Integer>>();
+        for(int i=0;i<arr.length;i++)
+        {
+            if(i<arr.length-1 && i>0 && arr[i+1]==arr[i] && arr[i-1]==arr[i])
+                continue;
+           if(hm.containsKey(arr[i]))
+           {  
+               hm.get(arr[i]).add(i);
+            }
+           else 
+            {
+               List<Integer> temp = new ArrayList<Integer>();
+               temp.add(i);
+               hm.put(arr[i],temp);
+            }
+            
+        }
+        boolean visited[] = new boolean[arr.length];
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(0);
+        visited[0] = true;
+        int min = 0;
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int i=0;i<size;i++)
+            {
+               int count = q.poll();
+            if(count==arr.length-1)
+                return min;
+            if(count<arr.length-1 && !visited[count+1])
+            {
+                visited[count+1]=true;
+                q.add(count+1);
+            }
+            if(count>0 && !visited[count-1])
+            {
+                visited[count-1]=true;
+                q.add(count-1);
+            }
+            if(hm.containsKey(arr[count]))
+            {
+                for(int j : hm.get(arr[count]))
+                {
+                    if(!visited[j])
+                    {
+                        q.add(j);
+                        visited[j] = true;
+                    }
+                }
+            }
+            hm.remove(arr[count]);
+        }
+            min++;  
+             
+            
+        }
+        return -1;
+    }
+}
