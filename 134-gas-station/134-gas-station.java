77725477b0bcd779ad4c8b738cd25d932faec4fa
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2022-01-21 12:41:57 +0530

    Time: 2 ms (65.41%), Space: 64.2 MB (14.00%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,19 +1,22 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-       int start = 0;
         int tank = 0;
-        int shortage = 0;
-       for(int i=0;i<gas.length;i++)
-       {
-           tank = tank + gas[i] - cost[i];
-           if(tank<0)
-           {
-               shortage += tank;
-               start = i+1;
-               tank = 0;
-           }
-       }
-      return (shortage+tank)>=0?start:-1;
+        int req = 0;
+        int start = 0;
+        
+        for(int i=0;i<gas.length;i++)
+        {
+            tank += gas[i] - cost[i];
+            if(tank<0)
+            {
+                start = i+1;
+                req += tank;
+                tank = 0;
+            }
+            
+            
+        }
+        return tank+req>=0?start:-1;
         
     }
 }
