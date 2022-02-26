721d433a7ef5015e0a622155cae7a4727a334d65
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2022-02-26 21:47:38 +0530

    Time: 97 ms (77.15%), Space: 99.6 MB (24.95%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
@@ -1,36 +1,28 @@
 class Solution {
     public int maxEvents(int[][] events) {
-        // edge, perf
-        // greedy. for any day, pick the event that ends soon.
-        // first, sort by start day.
-        Arrays.sort(events, (a, b) -> a[0] - b[0]);        
-        // then, we use pq - this will store end dates.
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int ei = 0;
-        int sd = events[ei][0];
-        int ret = 0;
-        while(pq.isEmpty() == false || ei < events.length) {
-            // put end days of events that start on sd.
-            while (ei < events.length && events[ei][0] == sd) {
-                pq.offer(events[ei][1]);
-                ei++;
-            }
-            
-            // attend an event on day sd.
-            if (!pq.isEmpty()) {
-                pq.poll();
-                ret++;
-                // remove events that ends on day sd.
-                // also if earliest ed is found, assign it to sd.
-                while(!pq.isEmpty()) {
-                    if (pq.peek() == sd)
-                        pq.poll();
-                    else
-                        break;
-                }
-            }
-            sd++;
-        }
-        return ret;
+       Arrays.sort(events,(t1,t2) -> t1[0]-t2[0]);
+       int day=0;
+       int count=0;
+       int i=0;
+       PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+       while(i<events.length || !pq.isEmpty())
+       {
+           if(pq.isEmpty())
+           {
+               day = events[i][0];
+           }
+           
+           while(i<events.length && events[i][0]==day)
+           {
+               pq.add(events[i][1]);
+               i++;
+           }
+           pq.poll();
+           day++;
+           count++;
+           while(!pq.isEmpty() && pq.peek()<day)
+               pq.poll();
+       }
+        return count;
     }
 }
