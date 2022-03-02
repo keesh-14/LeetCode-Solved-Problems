d1a4cc3921cf325f4dc9f05078d133a13af8257d
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2022-03-02 13:08:18 +0530

    Time: 7 ms (9.54%), Space: 43 MB (5.48%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+         int dp[][] = new int[s.length()+1][t.length()+1];
+         for(int i=1;i<s.length()+1;i++)
+         {
+             for(int j=1;j<t.length()+1;j++)
+             {
+                 if(s.charAt(i-1)==t.charAt(j-1))
+                     dp[i][j] = dp[i-1][j-1]+1;
+                 else 
+                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+             }
+         }
+        if(s.length()==dp[s.length()][t.length()])
+            return true;
+        return false;
+    }
+}
