09593df85b5931043ceddcaf3235a6e780a86c98
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2022-03-20 16:56:13 +0530

    Time: 5 ms (79.08%), Space: 91.2 MB (43.65%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,24 +1,28 @@
 class Solution {
-    public int minDominoRotations(int[] A, int[] B) {
-        int len  = A.length;
-        int[] countSum = new int[7], countA = new int[7], countB = new int[7];
-		
-        for(int i = 0 ; i < len; i++) {
-            if(A[i] == B[i]) 
-                countSum[A[i]]++;
-            else {
-                countSum[A[i]]++;
-                countSum[B[i]]++;
-                countA[A[i]]++;
-                countB[B[i]]++;
-            }
-        }
-        
-        for(int i = 1; i <= 6; i++) {
-            if(countSum[i] == len) 
-                return Math.min(countA[i], countB[i]);
-        }
-        
-        return -1; 
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int A[] = new int[7];
+       int B[] = new int[7];
+       int count[] = new int[7];
+       for(int i=0;i<bottoms.length;i++)
+       {
+           if(tops[i]==bottoms[i])
+           {
+               count[tops[i]]++;
+           }
+           else 
+           {
+               count[tops[i]]++;
+               count[bottoms[i]]++;
+               A[tops[i]]++;
+               B[bottoms[i]]++;
+           }
+           
+       }
+       for(int i=1;i<=6;i++)
+       {
+           if(count[i]==tops.length)
+           return Math.min(A[i],B[i]);
+       }
+       return -1; 
     }
 }
