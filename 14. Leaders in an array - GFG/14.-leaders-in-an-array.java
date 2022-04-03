c8b885008209c5f15f2ed6537b936655deafa08f
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2022-04-03 19:20:57 +0530

    Added solution - LeetHub

---
+++ 14. Leaders in an array - GFG/14.-leaders-in-an-array.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+		    //appending result to a String
+		    for(int i=0; i<res.size(); i++){
+		        str.append(res.get(i)+" ");
+		    }
+		    
+		    //printing the String
+		    System.out.println(str);
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        Stack<Integer> st = new Stack<Integer>();
+        int current = arr[arr.length-1];
+        st.add(current);
+        for(int i=arr.length-2;i>=0;i--)
+        {
+            if(arr[i]>=current)
+            {
+              st.add(arr[i]);
+              current = arr[i];
+            }
+        }
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        int t = st.size();
+        for(int i=0;i<t;i++)
+        {
+            al.add(st.pop());
+        }
+        return al;
+    }
+}
