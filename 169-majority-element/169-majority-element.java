738ac7f94f6c9264c29e13afb2e1f74a3c411154
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2022-04-05 01:11:22 +0530

    Time: 1 ms (99.96%), Space: 57.5 MB (5.76%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,34 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int a = nums[0];
+        int currentMax = nums[0];
         int count = 1;
         for(int i=1;i<nums.length;i++)
         {
-               if(nums[i]==a)
-               {
-                   count++;
-               }
-               else 
-               {
-                   count--;
-               }
-            
+            if(nums[i]==currentMax)
+                count++;
+            else 
+                count--;
             if(count==0)
             {
-                a = nums[i];
+                currentMax = nums[i];
                 count = 1;
             }
             
         }
-        count=0;
-      for(int i=0;i<nums.length;i++)
-      {
-          if(nums[i]==a)
-              count++;
-      }
-          
-        if(nums.length/2<count)
-            return a;
-        return -1;
+        return currentMax;
     }
 }
