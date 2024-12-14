30cd5036d3a9369cf60c36b1014d49c84771a0cf
keesh-14 <62787925+keesh-14@users.noreply.github.com>
2024-12-14 21:16:49 +0530

    Time: 4 ms, Memory: 44.9 MB - LeetHub

--- 0two-sum/0two-sum.java
+++ 0two-sum/0two-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
-        for(int i=0;i<nums.length; i++){
-             hmap.put(nums[i], i);
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        for(int i=0; i<nums.length;i++){
+            hmap.put(nums[i], i);
         }
         
         for(int i=0;i<nums.length;i++){
-            if(hmap.containsKey(target - nums[i]) && i!=hmap.get(target - nums[i])){
-                return new int[]{i, hmap.get(target - nums[i])};
-            }    
+            if(hmap.containsKey(target-nums[i]) && hmap.get(target-nums[i])!=i){
+                return new int[]{hmap.get(target-nums[i]), i};
+            }
         }
-        return new int[]{-1,-1};
+        return new int[]{-1, -1};
     }
 }
